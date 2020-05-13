4fa00ca8d463768f7c5f5227ac149c7bc30c31a5
noellora <noelrlora@gmail.com>
2020-05-12 20:48:14 -0700

    Test files

---
+++ lol/lol WatchKit Extension/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  lol WatchKit Extension
+//
+//  Created by Noel Lora on 5/12/20.
+//  Copyright © 2020 Noel Lora. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World! Theres a ton of filews in hrere and I hope tha ti will be able to create this app in the feature.")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
